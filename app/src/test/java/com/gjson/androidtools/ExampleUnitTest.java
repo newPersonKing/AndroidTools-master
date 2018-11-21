b07ad02232db37ed9c8193e531ecf9cc4ffdcba9
guoyong <guoyong@emcc.net.com>
2018-11-21 16:06:30 +0800

    tools

---
+++ app/src/test/java/com/gjson/androidtools/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+package com.gjson.androidtools;
+
+import com.gjson.androidtools.utils.EncodeUtil;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testGetMd5(){
+        EncodeUtil.urlDecode("httpllkll:kfjkfjlsf");
+    }
+}
